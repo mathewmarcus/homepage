7f3513e8d7c7eccc6fa601dd2116eebdbf344a16
mathewmarcus <mathewmarcus456@gmail.com>
2018-05-15 20:14:20 -0500

    added hover highlighting and hrefs

--- site-content/js/site.js
+++ site-content/js/site.js
@@ -1,21 +1,29 @@
 function collapseNav(innerWidth) {
-    var nav_collapse = document.getElementById("nav-collapse");
+    var nav_collapse = document.getElementById('nav-collapse');
     if (innerWidth.matches) {
-        nav_collapse.style.height = "0px";
+        nav_collapse.style.height = '0px';
     }
 }
 
 function toggleNav() {
-    var nav_collapse = document.getElementById("nav-collapse");
+    var nav_collapse = document.getElementById('nav-collapse');
 
-    if (nav_collapse.style.height === "0px") {
+    if (nav_collapse.style.height === '0px') {
         nav_collapse.style.height = nav_collapse.scrollHeight + 'px';
     }
     else {
-        nav_collapse.style.height = "0px";
+        nav_collapse.style.height = '0px';
+    }
+}
+
+function add_email() {
+    email_social_a = document.getElementsByClassName('social_email')
+    for(var i = 0; i < email_social_a.length; i++) {
+	email_social_a[i].setAttribute('href', 'mailto:' + 'mathewmarcus456' + '@' + 'gmail.com')
     }
 }
 
 hljs.initHighlightingOnLoad();
-document.getElementById("nav-collapse").style.height = "0px";
-window.matchMedia("(min-width: 600px)").addListener(collapseNav)
+document.getElementById('nav-collapse').style.height = '0px';
+window.matchMedia('(min-width: 600px)').addListener(collapseNav)
+document.addEventListener('DOMContentLoaded', add_email)
